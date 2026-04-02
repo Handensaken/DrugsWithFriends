b43cadca1f512dd1ebfadef245c1d103d6f90765
a23hanpu <144058026+a23hanpu@users.noreply.github.com>
2026-04-02 13:47:40 +0200

    Finally working

--- Assets/Scripts/BootstrapManager.cs
+++ Assets/Scripts/BootstrapManager.cs
@@ -47,7 +47,7 @@ public class BootstrapManager : MonoBehaviour
     
     public static void CreateLobby()
     {
-        SteamMatchmaking.CreateLobby(ELobbyType.k_ELobbyTypeFriendsOnly, 4);
+        SteamMatchmaking.CreateLobby(ELobbyType.k_ELobbyTypePublic, 4);
     }
 
     private void OnLobbyCreated(LobbyCreated_t callback)
