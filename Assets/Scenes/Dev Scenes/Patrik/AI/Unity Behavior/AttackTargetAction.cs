7cbf6954dcb2246864fd659871ea5e65c23f3340
Patrik <149394492+Kroonan02@users.noreply.github.com>
2026-04-23 15:01:52 +0200

    utslängd från sal =(

--- Assets/Scenes/Dev Scenes/Patrik/AI/Unity Behavior/AttackTargetAction.cs
+++ Assets/Scenes/Dev Scenes/Patrik/AI/Unity Behavior/AttackTargetAction.cs
@@ -1,26 +1,34 @@
 using System;
+using System.Diagnostics;
 using Unity.Behavior;
 using UnityEngine;
 using Action = Unity.Behavior.Action;
 using Unity.Properties;
+using Debug = UnityEngine.Debug;
 
 [Serializable, GeneratePropertyBag]
-[NodeDescription(name: "Attack Target", story: "Attack [targetPoint]", category: "Action/Interaction", id: "38191b92b81f9f5926eba4942ce86359")]
+[NodeDescription(name: "Attack Target", story: "Attack with [Collider]", category: "Action/Interaction", id: "38191b92b81f9f5926eba4942ce86359")]
 public partial class AttackTargetAction : Action
 {
-    [SerializeReference] public BlackboardVariable<Vector3> TargetPoint;
+    [SerializeReference] public BlackboardVariable<GameObject> collider;
+    [SerializeReference] public BlackboardVariable<float> time;
+    
+    private float _currentTime = 0;
     protected override Status OnStart()
     {
+        collider.Value.SetActive(true); 
         return Status.Running;
     }
 
     protected override Status OnUpdate()
     {
-        return Status.Success;
+        _currentTime += Time.deltaTime;
+        return Status.Running;
     }
 
     protected override void OnEnd()
     {
+        _currentTime = 0;
     }
 }
 
