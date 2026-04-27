d004405864f1c143aaf8cc1f5ac72549771428f1
Patrik <149394492+Kroonan02@users.noreply.github.com>
2026-04-27 21:41:41 +0200

    lite fix

--- Assets/Scripts/AI-Experimental/Unity Behavior/CustomActions/AttackAction.cs
+++ Assets/Scripts/AI-Experimental/Unity Behavior/CustomActions/AttackAction.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Diagnostics;
+using Scenes.Dev_Scenes.Patrik.AI.Extra;
 using Unity.Behavior;
 using UnityEngine;
 using Action = Unity.Behavior.Action;
@@ -11,29 +12,49 @@ using Debug = UnityEngine.Debug;
 public partial class AttackAction : Action
 {
     [SerializeReference] public BlackboardVariable<GameObject> collider;
-    [SerializeReference] public BlackboardVariable<float> time;
     
+    [SerializeReference] public BlackboardVariable<EnemyData> enemyData;
+
+
+    private float _startColliderTime;
+    private float _endColliderTime;
+    private float _totTime = 2;
     private float _currentTime = 0;
+    private bool _activeCollider = false;
     protected override Status OnStart()
     {
-        collider.Value.SetActive(true); 
+        //TODO activate trigger animation
         return Status.Running;
     }
 
     protected override Status OnUpdate()
     {
         _currentTime += Time.deltaTime;
-        if (_currentTime >= time)
+        if (!_activeCollider && _currentTime <= _endColliderTime)
+        {
+            collider.Value.SetActive(true); 
+            _activeCollider = true;
+        }
+        else if (_activeCollider && _currentTime >= _endColliderTime)
         {
             collider.Value.SetActive(false); 
-            return Status.Success;
         }
+        
+        /*
+         * Animation done --> return succesfull
+         */
         return Status.Running;
     }
 
     protected override void OnEnd()
     {
+        collider.Value.SetActive(false); 
         _currentTime = 0;
     }
+}
+
+public struct ITimeData
+{
+    
 }
 
