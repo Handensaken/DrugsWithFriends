07df7471d2934deec7e942d0796f3d95cdf2b555
Patrik <149394492+Kroonan02@users.noreply.github.com>
2026-04-27 09:57:01 +0200

    Update NullCheckCondition.cs

--- Assets/Scenes/Dev Scenes/Patrik/AI/Unity Behavior/CustomCondition/NullCheckCondition.cs
+++ Assets/Scenes/Dev Scenes/Patrik/AI/Unity Behavior/CustomCondition/NullCheckCondition.cs
@@ -14,6 +14,6 @@ public partial class NullCheckCondition : Condition
 
     public override bool IsTrue()
     {
-        return inverse ? !Variable.ObjectValue.Equals(null) : Variable.ObjectValue.Equals(null);
+        return inverse ? !Variable.ObjectValue.Equals(null) || !Variable.Equals(null) : Variable.ObjectValue.Equals(null) || Variable.Equals(null);
     }
 }
