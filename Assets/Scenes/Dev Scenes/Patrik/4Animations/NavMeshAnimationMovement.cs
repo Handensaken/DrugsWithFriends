7ac8d897617cfcbfdc98f9b138a0a75913a94d45
Kroonan02 <149394492+Kroonan02@users.noreply.github.com>
2026-04-28 12:07:25 +0200

    struktur och navmesh parametrar till animation på g

---
+++ Assets/Scenes/Dev Scenes/Patrik/4Animations/NavMeshAnimationMovement.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Scenes.Dev_Scenes.Patrik._4Animations
+{
+    public class NavMeshAnimationMovement : MonoBehaviour
+    {
+        //[SerializeField] private string 
+        
+        [Space,Header("References"),SerializeField] private NavMeshAgent agent;
+        [SerializeField] private Animator animator;
+
+    }
+}
