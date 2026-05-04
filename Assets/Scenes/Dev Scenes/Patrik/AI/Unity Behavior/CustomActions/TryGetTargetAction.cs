93ba13afa0fa9b80b8002c093e114b82034ca1a4
Patrik <149394492+Kroonan02@users.noreply.github.com>
2026-05-04 14:39:57 +0200

    Fixade mer med konstiga errors i unity behavior

--- Assets/Scenes/Dev Scenes/Patrik/AI/Unity Behavior/CustomActions/TryGetTargetAction.cs
+++ Assets/Scenes/Dev Scenes/Patrik/AI/Unity Behavior/CustomActions/TryGetTargetAction.cs
@@ -1,62 +1,32 @@
 using System;
-using Unity.Behavior;
-using UnityEngine;
-using Action = Unity.Behavior.Action;
-using Unity.Properties;
-
-[Serializable, GeneratePropertyBag]
-[NodeDescription(name: "TryGetTarget", story: "Get suitable [target] from [allTargets]", category: "Action/Interaction", id: "00d6238e85e494466c19dbe18182faaa")]
-public partial class TryGetTargetAction : Action
-{
-    [SerializeReference] public BlackboardVariable<Transform> Target;
-    [SerializeReference] public BlackboardVariable<GameObject> AllTargets;
-
-    protected override Status OnStart()
-    {
-        return Status.Running;
-    }
-
-    protected override Status OnUpdate()
-    {
-        return Status.Success;
-    }
-
-    protected override void OnEnd()
-    {
-    }
-}
-/*
-using System;
 using System.Collections.Generic;
-using System.IO;
-using FishNet;
-using FishNet.Connection;
 using Unity.Behavior;
 using UnityEngine;
 using Action = Unity.Behavior.Action;
 using Unity.Properties;
-using Unity.VisualScripting;
 using UnityEngine.AI;
 
 [Serializable, GeneratePropertyBag]
-[NodeDescription(name: "AcquireTarget", story: "Get New [Target] from [AllTargets]", category: "Action/Interaction", id: "db4d9d13cdf182cf25727bb4e4ccd941")]
-public partial class AcquireTargetAction : Action
+[NodeDescription(name: "TryGetTarget", story: "Get suitable [target] from [allTargets]", category: "Action/Interaction",
+    id: "00d6238e85e494466c19dbe18182faaa")]
+public partial class TryGetTargetAction : Action
 {
-    [SerializeReference] public BlackboardVariable<GameObject[]> AllTargets;
     [SerializeReference] public BlackboardVariable<Transform> Target;
-    
+    [SerializeReference] public BlackboardVariable<List<GameObject>> AllTargets;
+
     [SerializeReference] public BlackboardVariable<GameObject> self;
-    
-    /*private NavMeshAgent _agent;
+
+    private NavMeshAgent _agent;
+
     protected override Status OnStart()
     {
         _agent = self.Value.GetComponent<NavMeshAgent>();
 
-        if (allTargets.Value.Length <= 0)
+        if (AllTargets.Value.Count <= 0)
         {
             return Status.Failure;
         }
-        
+
         return Status.Running;
     }
 
@@ -78,7 +48,7 @@ public partial class AcquireTargetAction : Action
     private float UtilityDistanceValue(Vector3 targetPosition)
     {
         _agent.SetDestination(targetPosition);
-        
+
         NavMeshPath path = new NavMeshPath();
         _agent.CalculatePath(targetPosition, path); //TODO can be heavy on performance
 
@@ -86,28 +56,31 @@ public partial class AcquireTargetAction : Action
         {
             Debug.Log(_agent.remainingDistance);
         }
+
         return 0;
-    }  
+    }
+
     private float UtilityMaxHealthValue()
     {
         return 0;
-    }  
+    }
+
     private float UtilityCurrentHealthValue()
     {
         return 0;
-    }  
+    }
 
     private Transform EvaluateAll()
     {
-        List< Tuple<float,Transform>> evaluationValueAndTransform = new List<Tuple<float, Transform>>();
+        List<Tuple<float, Transform>> evaluationValueAndTransform = new List<Tuple<float, Transform>>();
 
-        foreach (var target in allTargets.Value)
+        foreach (var target in AllTargets.Value)
         {
             float sum = 0;
             sum += UtilityDistanceValue(target.transform.position);
             sum += UtilityMaxHealthValue();
             sum += UtilityCurrentHealthValue();
-            
+
             evaluationValueAndTransform.Add(new Tuple<float, Transform>(sum, target.transform));
         }
 
@@ -122,17 +95,14 @@ public partial class AcquireTargetAction : Action
 
             float currentEvaluationValue = currentBest.Item1;
             float nextEvaluationValue = valueTransform.Item1;
-            
+
             if (nextEvaluationValue > currentEvaluationValue)
             {
                 currentBest = valueTransform;
                 continue;
             }
         }
-        
+
         return currentBest.Item2;
     }
-*/
-
-
-
+}
---
+++ Assets/Scripts/AI-Experimental/Unity Behavior/ExternalComponents/ExternalTargetHandler.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using FishNet.Object;
+
+public class ExternalTargetHandler : NetworkBehaviour
+{
+     private void Awake() { }
+
+     private void Update() { }
+}
