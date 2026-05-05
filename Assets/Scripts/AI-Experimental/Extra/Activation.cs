18fbc99bba38317e5145896c51ec69cac5e27052
Kroonan02 <149394492+Kroonan02@users.noreply.github.com>
2026-05-05 14:50:04 +0200

    AnimationEvent - struktur

---
+++ Assets/Scripts/AI-Experimental/Extra/Activation.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace AI_Experimental.Extra
+{
+    public class Activation : MonoBehaviour
+    {
+        [SerializeField] private GameObject gameObject;
+
+        public void Activate()
+        {
+            Debug.Log("activation");
+            gameObject.SetActive(true);
+        }
+
+        public void Deactivate()
+        {
+            Debug.Log("deactivation");
+            gameObject.SetActive(false);
+        }
+    
+    }
+}
