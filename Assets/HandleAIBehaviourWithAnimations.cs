ff76a6544e434e7684aa7e5ed54323df713130af
Kroonan02 <149394492+Kroonan02@users.noreply.github.com>
2026-05-05 15:45:45 +0200

    mer animation

--- Assets/HandleAIBehaviourWithAnimations.cs
+++ Assets/HandleAIBehaviourWithAnimations.cs
@@ -1,11 +1,14 @@
 using Unity.Behavior;
 using UnityEngine;
 
+[Tooltip("Will change if requested - not a good solution")]
 public class HandleAIBehaviourWithAnimations : StateMachineBehaviour
 {
+    [SerializeField] private string boolName;
+    [SerializeField] private bool value;
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.GetComponentInParent<BehaviorGraphAgent>().SetVariableValue("Attack", false);
+        animator.GetComponentInParent<BehaviorGraphAgent>().SetVariableValue(boolName, value);
     }
 }
