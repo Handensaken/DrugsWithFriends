371f4fded0fa01f96d8352a5d769c158747a9b15
a23hanpu <144058026+a23hanpu@users.noreply.github.com>
2026-05-18 13:14:58 +0200

    Fixes

--- Assets/Scripts/UI/ResolutionSettings.cs
+++ Assets/Scripts/UI/ResolutionSettings.cs
@@ -9,10 +9,12 @@ public class ResolutionSettings : MonoBehaviour
     private List<string> resolutionsList;
     private int selectedResolution;
 
-    private FullScreenMode fullscreenMode = FullScreenMode.FullScreenWindow;
+    private FullScreenMode fullscreenMode;
 
     private void Awake()
     {
+        fullscreenMode = Screen.fullScreenMode;
+
         resolutions = Screen.resolutions;
         resolutionsList = new List<string>();
 
@@ -24,21 +26,26 @@ public class ResolutionSettings : MonoBehaviour
         }
 
         resolutionDropdown.options = resolutionsList.ConvertAll(res => new TMP_Dropdown.OptionData(res));
+        
+        string currentRes = $"{Screen.width} x {Screen.height}";
+        selectedResolution = resolutionsList.IndexOf(currentRes);
+        if (selectedResolution < 0) selectedResolution = 0;
 
+        resolutionDropdown.SetValueWithoutNotify(selectedResolution);
+        
         List<string> screenModesList = new List<string>();
-
         foreach (FullScreenMode mode in System.Enum.GetValues(typeof(FullScreenMode)))
-        {
             screenModesList.Add(mode.ToString());
-        }
 
         screenModeDropdown.options = screenModesList.ConvertAll(mode => new TMP_Dropdown.OptionData(mode));
-        screenModeDropdown.value = screenModesList.IndexOf(fullscreenMode.ToString());
+        
+        int currentModeIndex = screenModesList.IndexOf(fullscreenMode.ToString());
+        screenModeDropdown.SetValueWithoutNotify(currentModeIndex >= 0 ? currentModeIndex : 0);
     }
 
-    public void ChangeResolution()
+    public void ChangeResolution(int index)
     {
-        selectedResolution = resolutionDropdown.value;
+        selectedResolution = index;
         Screen.SetResolution(resolutions[selectedResolution].width, resolutions[selectedResolution].height, fullscreenMode);
     }
 
